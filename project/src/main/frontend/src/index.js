940f92dc055ef6f5b31324cb0b364348f100b523
유은철 <isz06188@iansit.co.kr>
2024-08-23 15:08:06 +0900

    ㅅㅅㅅ

--- project/src/main/frontend/src/index.js
+++ project/src/main/frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './assets/styles/index.css';
+import App from './components/app/App';
+import reportWebVitals from './utils/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
