0de776cb446150ff111616148b93a2fddf1dcded
유은철 <isz06188@iansit.co.kr>
2024-09-05 16:45:17 +0900

    asd

--- project/src/main/java/cap/project/controller/HelloApiController.java
+++ project/src/main/java/cap/project/controller/HelloApiController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class HelloController {
+public class HelloApiController {
     
     
     @GetMapping("/api/hello")
